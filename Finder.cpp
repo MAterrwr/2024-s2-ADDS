8a76074f7e0f420f5626a1fa88c028825a2abc45
MAterrwr <empororhotdowg21@empororhotdowg2>
2024-08-25 13:13:42 +0930

    gg

--- Finder.cpp
+++ Finder.cpp
@@ -4,17 +4,16 @@
 
     vector<int> Finder::findSubstrings(string s1, string s2) {
     vector<int> result;
-    size_t index = 0;  // Start searching from the beginning of s1
+    size_t index = 0;  
     for (size_t i = 1; i <= s2.size(); ++i) {
         index = s1.find(s2.substr(0, i), index);
         if (index != string::npos) {
             result.push_back(index);
         } else {
             result.push_back(-1);
-            break;  // No need to search for longer prefixes if this one isn't found
+            break; 
         }
     }
-    // For remaining lengths of s2 that were not processed due to break
     for (size_t j = result.size(); j < s2.size(); ++j) {
         result.push_back(-1);
     }
